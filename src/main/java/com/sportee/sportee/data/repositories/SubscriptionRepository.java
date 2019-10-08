2e0f50b170bc1e571a6f91f9b366599d90bbee34
cosmiailinei <cosmina.ailinei@gmail.com>
2019-10-08 18:32:01 +0300

    Revert "Subscription Service, Controller, HTML"
    This reverts commit acca9914

--- src/main/java/com/sportee/sportee/data/repositories/SubscriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sportee.sportee.data.repositories;
-
-import com.sportee.sportee.data.DAO.Subscription;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubscriptionRepository extends CrudRepository<Subscription,Integer> {
-}
--- src/main/java/com/sportee/sportee/services/ISubscriptionService.java
+++
@@ -1,9 +0,0 @@
-package com.sportee.sportee.services;
-
-import com.sportee.sportee.data.DTO.SubscriptionDTO;
-
-import java.util.List;
-
-public interface ISubscriptionService {
-    List<SubscriptionDTO>getAllSubscription();
-}
