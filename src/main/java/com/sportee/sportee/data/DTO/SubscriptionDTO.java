49b6977edb2d552e3f31068d5bf5eb92d933f920
Ralu <roxanamustata@yahoo.com>
2019-10-08 18:51:41 +0300

    Add SubscriptionDTO

---
+++ src/main/java/com/sportee/sportee/data/DTO/SubscriptionDTO.java
@@ -0,0 +1,25 @@
+package com.sportee.sportee.data.DTO;
+
+import com.sportee.sportee.data.DAO.SporteeMember;
+import com.sportee.sportee.data.DAO.Subscription;
+import com.sportee.sportee.data.DAO.SubscriptionType;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+public class SubscriptionDTO {
+
+    private Date date;
+    private boolean valid;
+    private String subscriptionType;
+    private String sporteeMember;
+
+    public SubscriptionDTO(Subscription subscription) {
+        this.date = subscription.getDate();
+        this.valid = subscription.isValid();
+        this.subscriptionType = subscription.getSubscriptionType().getName();
+        this.sporteeMember = subscription.getSporteeMember().getFirstName() + " " +
+                subscription.getSporteeMember().getLastName();
+    }
+}
