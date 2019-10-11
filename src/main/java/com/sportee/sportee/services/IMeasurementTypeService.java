7b9a9687bf4e3d2a6d06d424b20c21114901c376
Ralu <roxanamustata@yahoo.com>
2019-10-11 18:48:36 +0300

    Add "insert/delete" to measurement types

--- src/main/java/com/sportee/sportee/services/IMeasurementTypeService.java
+++ src/main/java/com/sportee/sportee/services/IMeasurementTypeService.java
@@ -2,8 +2,13 @@ package com.sportee.sportee.services;
 
 import com.sportee.sportee.data.DTO.MeasurementTypeDTO;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface IMeasurementTypeService {
     List<MeasurementTypeDTO> getAllMeasurementTypes();
+
+    void insertMeasurementType(String name, String unit);
+
+    void deleteMeasurementType(Integer id);
 }
